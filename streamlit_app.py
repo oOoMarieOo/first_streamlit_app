98c4ff64ff0e866f7d3a35ed223eb266823428a6
oOoMarieOo <58491238+oOoMarieOo@users.noreply.github.com>
2022-09-13 11:45:22 +0200

    error

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
-streamlist.text(pandas.json_normalize(fruityvice_response.json()))
+streamlit.text(pandas.json_normalize(fruityvice_response.json()))
 
 # converts json to array/list
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
